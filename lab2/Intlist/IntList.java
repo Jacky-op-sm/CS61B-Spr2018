e50e5a4746a2dd9b2e443ccf3f7dff680679cdc3
Jacky <jackylin714@gmail.com>
2020-05-12 22:01:41 +0800

    lab2 has been finished

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,6 +83,9 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         IntList p = A;
+        if (p == null) {
+        	return B;
+        }
         while (p.rest != null) {
         	p = p.rest;
         }
