b3cdbdf2a00c4bc4f56320fdd08508fd573062a7
Jacky <jackylin714@gmail.com>
2020-06-11 22:08:52 +0800

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,151 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF wqu;
+    private boolean[] grid;
+    private int n;
+    private int size;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        validate(N);
+        this.n = N;
+        size = 0;
+        int length = N * N + 2;
+        wqu = new WeightedQuickUnionUF(length);
+        grid = new boolean[length];
+        grid[0] = true;
+        grid[length - 1] = true;
+        for (int i = 1; i <= length - 2; i += 1) {
+            grid[i] = false;
+        }
+        for (int i = 1; i <= N; i += 1) {
+            wqu.union(0, i);
+        }
+        for (int i = N * N - N + 1; i <= N * N; i += 1) {
+            wqu.union(N * N + 1, i);
+        }
+    }
+
+    // use for unit testing (not required)
+    public static void main(String[] args) {
+        Percolation test = new Percolation(5);
+        test.open(3, 4);
+        test.open(2, 4);
+        test.open(2, 2);
+        test.open(2, 3);
+        test.open(0, 2);
+        test.open(1, 2);
+        test.open(4, 4);
+        System.out.println(test.isFull(4, 4));
+        System.out.println(test.percolates());
+        System.out.println(test.numberOfOpenSites());
+        System.out.println((double) 7);
+    }
+
+    private int xyTo1D(int row, int col) {
+        return n * row + col + 1;
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validate(row, col);
+        int num = xyTo1D(row, col);
+        if (!grid[num]) {
+            grid[num] = true;
+            size += 1;
+            if (row == 0) {
+                connectDown(row, col);
+            } else if (row == n - 1) {
+                connectUp(row, col);
+            } else {
+                connectDown(row, col);
+                connectUp(row, col);
+            }
+            if (col == 0) {
+                connectRight(row, col);
+            } else if (col == n - 1) {
+                connectLeft(row, col);
+            } else {
+                connectRight(row, col);
+                connectLeft(row, col);
+            }
+        }
+    }
+
+    private void connectDown(int row, int col) {
+        if (isOpen(row + 1, col)) {
+            int i = xyTo1D(row, col);
+            int k = xyTo1D(row + 1, col);
+            wqu.union(i, k);
+        }
+    }
+
+    private void connectLeft(int row, int col) {
+        if (isOpen(row, col - 1)) {
+            int i = xyTo1D(row, col);
+            int k = xyTo1D(row, col - 1);
+            wqu.union(i, k);
+        }
+    }
+
+    private void connectUp(int row, int col) {
+        if (isOpen(row - 1, col)) {
+            int i = xyTo1D(row, col);
+            int k = xyTo1D(row - 1, col);
+            wqu.union(i, k);
+        }
+    }
+
+    private void connectRight(int row, int col) {
+        if (isOpen(row, col + 1)) {
+            int i = xyTo1D(row, col);
+            int k = xyTo1D(row, col + 1);
+            wqu.union(i, k);
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        int num = xyTo1D(row, col);
+        return grid[num];
+    }
+
+    // validate that (row, col) is a valid
+    private void validate(int row, int col) {
+        if (row < 0 || row >= n || col < 0 || col >= n) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
+
+    // validate that the size of grid is positive
+    private void validate(int k) {
+        if (k <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        int num = xyTo1D(row, col);
+        if (isOpen(row, col)) {
+            return wqu.connected(num, 0);
+        }
+        return false;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return size;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return wqu.connected(0, n * n + 1);
+    }
 
 }
+
+
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,59 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
 
+    double[] stats;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        validate(N, T);
+        stats = new double[T];
+        for (int i = 0; i < T; i += 1) {
+            Percolation exp = pf.make(N);
+            while (!exp.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                exp.open(row, col);
+            }
+            double openRatio = (double) exp.numberOfOpenSites() / (N * N);
+            stats[i] = openRatio;
+        }
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(stats);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(stats);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        double m = mean();
+        double s = stddev();
+        return m - 1.96 * s / (Math.sqrt(stats.length));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        double m = mean();
+        double s = stddev();
+        return m + 1.96 * s / (Math.sqrt(stats.length));
+    }
+
+    // validate that (row, col) is a valid
+    private void validate(int n, int t) {
+        if (n <= 0 || t <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+    }
 }
+
+
